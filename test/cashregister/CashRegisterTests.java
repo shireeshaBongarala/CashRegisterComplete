a5267e86e63acef2cc717667b6c2cb23f470b78e
Shireesha <bongaralashireesha@gmail.com>
2016-04-01 10:32:59 +0530

    Refactoring code using mockito mocks. Removed MockPrinter and PurchaseStub classes

--- test/cashregister/CashRegisterTests.java
+++ test/cashregister/CashRegisterTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class CashRegisterTests {
@@ -12,29 +13,29 @@ public class CashRegisterTests {
     @Mock
     Printer mockPrinter;
 
+    @Mock
+    Purchase mockPurchase;
+
     @Before
     public void setUp() {
         initMocks(this);
     }
     @Test
     public void shouldPrintThePurchaseInformation() {
-        MockPrinter mockPrinter = new MockPrinter();
         CashRegister cashRegister = new CashRegister(mockPrinter);
-        Item[] itemList = new Item[]{new Item("Book", 5)};
-        Purchase purchase = new Purchase(itemList);
 
-        cashRegister.process(purchase);
-
-        mockPrinter.verifyThatPrintIsCalledWithString(purchase.asString());
+        when(mockPurchase.asString()).thenReturn("Hello");
+        
+        cashRegister.process(mockPurchase);
+        verify(mockPrinter).print(mockPurchase.asString());
     }
 
     @Test
     public void shouldPrintThePurchaseInformationWithoutTestingPurchaseAsStringMethod(){
         CashRegister cashRegister = new CashRegister(mockPrinter);
-        PurchaseStub purchaseStub = new PurchaseStub();
 
-        cashRegister.process(purchaseStub);
+        cashRegister.process(mockPurchase);
 
-        verify(mockPrinter).print(purchaseStub.asString());
+        verify(mockPrinter).print(mockPurchase.asString());
     }
 }
